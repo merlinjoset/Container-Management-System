10b74b1938404e7d504d175923ed9f754cf5ef77
MerlinJose89 <t.merlin72@gmail.com>
2026-02-25 15:19:50 +0400

    update vessel

--- src/ContainerManagement.Application/Dtos/Vessels/VesselCreateDto.cs
+++ src/ContainerManagement.Application/Dtos/Vessels/VesselCreateDto.cs
@@ -5,12 +5,15 @@ namespace ContainerManagement.Application.Dtos.Vessels
     public class VesselCreateDto
     {
         [Required, StringLength(150)]
+        [Display(Name = "Vessel Name")]
         public string VesselName { get; set; }
 
         [Required, StringLength(50)]
+        [Display(Name = "Vessel Code")]
         public string VesselCode { get; set; }
 
         [Required, StringLength(20)]
+        [Display(Name = "IMO Code")]
         public string ImoCode { get; set; }
 
         public int? Teus { get; set; }
@@ -20,9 +23,10 @@ namespace ContainerManagement.Application.Dtos.Vessels
         [StringLength(100)]
         public string? Flag { get; set; }
         public decimal? Speed { get; set; }
+        
+         [Display(Name = "Build Year")]
         public int? BuildYear { get; set; }
 
         public Guid CreatedBy { get; set; }
     }
 }
-
